004fe2c180533c4f1a5d09593e49df6a862f0b91
= <=>
2019-01-28 22:38:13 -0700

    Add functionality for numbers to display when clicked on dialer

--- phone.js
+++ phone.js
@@ -21,3 +21,63 @@ $("#tab_3_button").click(function() { // when "tab_3" is clicked
 	$("#dialer").hide();
   $("#contacts").hide();
 });
+
+$("#one").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '1';
+});
+
+$("#two").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '2';
+});
+
+$("#three").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '3';
+});
+
+$("#four").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '4';
+});
+
+$("#five").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '5';
+});
+
+$("#six").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '6';
+});
+
+$("#seven").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '7';
+});
+
+$("#eight").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '8';
+});
+
+$("#nine").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '9';
+});
+
+$("#zero").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '0';
+});
+
+$("#star").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '*';
+});
+
+$("#pound").click(function() {
+  var text = document.getElementById('inputText');
+	inputText.value += '#';
+});
